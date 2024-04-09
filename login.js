d9d8ec655fba66762aff071c8641286aa4ffe9c1
Oluwafisayo-Eyitayo <lurveth@gmail.com>
2024-04-09 20:30:03 +0100

    changes

---
+++ login.js
@@ -0,0 +1,13 @@
+var firstName = document.getElementById("firstname");
+
+
+let userDetailsdatabase = {}
+
+function getuserDetails(){
+    var firstName = input.value;
+
+    if (firstName == null){
+        return
+    }
+}
+input.addEventListener("change", getuserDetails);
